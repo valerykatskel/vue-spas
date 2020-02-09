5ce3c9587f0ad06f32953fc2018b2654b8a7d05a
Valery Katskel <ne.vse.srazu@gmail.com>
2020-02-10 01:34:29 +0300

    customizing routes

---
+++ src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Navigation</div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
